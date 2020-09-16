fdb93dbeec6af8bf5069c8b6ec7d825dbcacb1a3
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-09-16 19:15:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -53,7 +53,7 @@ end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   "SELECT projects.category, SUM(pledges.amount) AS total_pledges FROM projects 
-  JOIN pledges ON pledges.project_id = project.id 
+  JOIN pledges ON pledges.project_id = projects.id 
   WHERE category = 'books'
   ";
 end
